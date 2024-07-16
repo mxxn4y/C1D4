d77ed96ef0ab59370c7429507aeb696aebd470b1
2yeb <qls7404@naver.com>
2024-07-16 22:37:23 +0900

    C1D4_ShopSystem

---
+++ Assets/02.Scripts/Shop/ShopItemSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "ShopMenu", menuName = "Scriptable Objects/New Shop Item", order = 1)]
+public class ShopItemSO : ScriptableObject
+{
+    public string title;
+    public int cost;
+    public Image itemImg;
+
+}
