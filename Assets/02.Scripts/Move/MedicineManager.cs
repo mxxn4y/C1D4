3942751ad808ee44aa975a0dc91ed210f55ace95
SwuChaeYeong <gkscodud0527@gmail.com>
2024-09-13 18:40:59 +0900

    Manager 추가
    GameManager, MedicineManager 추가, Tile, TileManager 삭제

---
+++ Assets/02.Scripts/Move/MedicineManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedicineManager : MonoBehaviour
+{
+    //약먹기
+    //약안먹기
+    //게임매니저에 약 먹은 카운트 올리기
+}
