3667794e2732abd6b78011afea0ca609b521dc3a
konguksu <rndbsk9@swu.ac.kr>
2024-11-11 22:39:49 +0900

    체력바&이벤트 버튼 수정, 미니언 클릭 상호작용 추가

--- Assets/02.Scripts/Card/Factory/Tile/MinionController.cs
+++ Assets/02.Scripts/Card/Factory/Tile/MinionController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -13,11 +14,12 @@ public class MinionController : MonoBehaviour
     #region Fields and Properties
     
     [SerializeField] private TileInfo tile;
-    [SerializeField] private GameObject minionGO;
+    [SerializeField] private GameObject minionGo;
     [SerializeField] private Image staminaBarPrefab;
     [SerializeField] private Button eventButtonPrefab;
     private Image staminaBar;
     private Button eventButton;
+    private TextMeshProUGUI eventBtnText;
     private readonly Vector3 staminaBarPos = new Vector3(0.5f,0.4f,0);
     private readonly Vector3 eventButtonPos = new Vector3(0f,1.0f,0);
     
@@ -27,90 +29,128 @@ public class MinionController : MonoBehaviour
         get { return currentStamina; } 
         set { 
             currentStamina = value;
-            SetHealthbar();
+            SetStaminaBar();
         } 
     }
     private bool isActive;
-    private float defaultTime; //일반재화 생산 주기
-    private float specialTime; //특수재화 생산 주기
-    private float eventTime; //상호작용 주기
+    private bool isResting;
+    private int restTimer; // 휴식 쿨타임
+    private float gemTimer; //일반재화 생산 주기
+    private float sGemAndStaminaTimer; //특수재화 생산 주기
+    private float gainStaminaTimer;
+    private float eventTimer; //상호작용 주기
+    private Coroutine[] eventCoroutine;
 
     #endregion
 
     #region Methods
     private void Awake()
     {
-        CardPlaceManager.Instance.OnCardPlace += ActivateMinion;
+        CardPlaceManager.Instance.OnCardPlace += SetMinion;
     }
 
     private void Start()
     {
+        minionGo.SetActive(false);
         isActive = false;
+        // 체력바 생성
+        staminaBar = Instantiate(staminaBarPrefab, 
+            Camera.main.WorldToScreenPoint(transform.position + staminaBarPos) , 
+                    Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
+        staminaBar.gameObject.SetActive(false);
+        // 이벤트 버튼 생성
+        eventButton = Instantiate(eventButtonPrefab, 
+            Camera.main.WorldToScreenPoint(transform.position + eventButtonPos),
+                    Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
+        eventBtnText = eventButton.GetComponentInChildren<TextMeshProUGUI>();
+        eventButton.gameObject.SetActive(false);
     }
     private void Update()
     {
         if (isActive)
         {
-            defaultTime += Time.deltaTime;
-            if (defaultTime >= minion.BaseData.speed)
+            gemTimer += Time.deltaTime;
+            if (gemTimer >= minion.BaseData.speed)
             {
-                defaultTime -= minion.BaseData.speed;
+                gemTimer -= minion.BaseData.speed;
                 FactoryManager.Instance.AddGem(minion.BaseData.efficiency);
             }
 
-            specialTime += Time.deltaTime;
-            if (specialTime >= 10.0f)
+            sGemAndStaminaTimer += Time.deltaTime;
+            if (sGemAndStaminaTimer >= 1.0f)
             {
-                specialTime -= 10.0f;
-                if(minion.TryEarnSpecialGem()) { FactoryManager.Instance.AddSpecialGem(); }
-                
-                if(--CurrentStamina <= 0)
+                sGemAndStaminaTimer -= 1.0f;
+                // 특수재화 생산 시도
+                if (minion.TryEarnSpecialGem())
+                {
+                    FactoryManager.Instance.AddSpecialGem();
+                }
+
+                // 체력 감소   
+                if (--CurrentStamina <= 0)
                 {
                     DeactivateMinion();
                 }
-
             }
-
+        }
+        else if (isResting)
+        {
+            gainStaminaTimer += Time.deltaTime;
+            if (gainStaminaTimer >= 1.0f)
+            {
+                gainStaminaTimer -= 1.0f;
+                CurrentStamina = Math.Min(CurrentStamina + 0.5f, minion.BaseData.stamina);
+                Debug.Log($"{minion.BaseData.mid}의 현재 체력: {CurrentStamina}");
+            }
         }
     }
     /// <summary>
     /// 선택된 카드가 해당 미니언이 존재하는 타일과 일치한다면 미니언 활성화
     /// </summary>
-    private void ActivateMinion()
+    private void SetMinion()
     {
         var placeManager = CardPlaceManager.Instance;
         if (placeManager.SelectedTile == tile)
         {  
-            minionGO.SetActive(true);
             minion = placeManager.SelectedCard.Minion;
-            staminaBar = Instantiate(staminaBarPrefab, 
-                Camera.main.WorldToScreenPoint(transform.position + staminaBarPos) , 
-                        Quaternion.identity, GameObject.FindGameObjectWithTag("StaminaCanvas").transform);
+            minionGo.SetActive(true);
+            staminaBar.gameObject.SetActive(true);
             CurrentStamina = minion.BaseData.stamina;
             SetImage();
-            isActive = true;
-            defaultTime = 0.0f;
-            specialTime = 0.0f;
+            eventCoroutine = new Coroutine[2];
             FactoryManager.Instance.minions.Add(this);
-            switch (FactoryManager.Instance.minions.Count)
+            eventTimer = FactoryManager.Instance.minions.Count switch
             {
-                case 1:
-                    eventTime = 2.0f;
-                    break;
-                case 2:
-                    eventTime = 3.0f;
-                    break;
-                case 3:
-                    eventTime = 5.0f;
-                    break;
-                case 4:
-                    eventTime = 7.0f;
-                    break;
-                default:
-                    eventTime = 10.0f;
-                    break;
-            }
-            StartCoroutine(MinionEvent(eventTime));
+                1 => 4.0f,
+                2 => 6.0f,
+                3 => 7.0f,
+                4 => 9.0f,
+                _ => 11.0f
+            };
+            ActivateMinion();
+        }
+    }
+
+    private void ActivateMinion()
+    {
+        isActive = true;
+        isResting = false;
+        gemTimer = 0.0f;
+        sGemAndStaminaTimer = 0.0f;
+        minionGo.GetComponent<SpriteRenderer>().color = Color.white;
+        eventCoroutine[0] = StartCoroutine(MinionEvent(eventTimer));
+    }
+
+    private void RestMinion()
+    {
+        isActive = false;
+        isResting = true;
+        gainStaminaTimer = 0.0f;
+        eventButton.gameObject.SetActive(false);
+        minionGo.GetComponent<SpriteRenderer>().color = Color.black;
+        foreach (var coroutine in eventCoroutine.Where(_c=>_c!=null))
+        {
+            StopCoroutine(coroutine);
         }
     }
 
@@ -118,26 +158,22 @@ public class MinionController : MonoBehaviour
     {
         isActive = false;
         tile.TileState = TILE_STATE.DEFAULT;
-        minionGO.SetActive(false);
-        Destroy(staminaBar.gameObject);
-        if (eventButton != null)
-        {
-            Destroy(eventButton.gameObject);
-        }
+        minionGo.SetActive(false);
+        staminaBar.gameObject.SetActive(false);
+        eventButton.gameObject.SetActive(false);
         StopAllCoroutines();
-
     }
 
     
 
-    private void SetHealthbar()
+    private void SetStaminaBar()
     {
         staminaBar.fillAmount = CurrentStamina / minion.BaseData.stamina;
     }
     
     //이미지 -> 애니메이션으로 넣으면 수정 필요
     /// <summary>
-    /// cid 값과 일치하는 이미지 할당
+    /// mid 값과 일치하는 이미지 할당
     /// </summary>
     private void SetImage()
     {
@@ -148,11 +184,12 @@ public class MinionController : MonoBehaviour
             return;
         }
 
+        var spriteRenderer = minionGo.GetComponent<SpriteRenderer>();
         foreach (var image in characterImages)
         {
             if (image.name == minion.BaseData.mid)
             {
-                minionGO.GetComponent<SpriteRenderer>().sprite =image;
+                spriteRenderer.sprite = image;
                 return;
             }
         }
@@ -163,55 +200,50 @@ public class MinionController : MonoBehaviour
 
     // 랜덤으로 선택된 상호작용 버튼을 띄우고 버튼을 누르거나 1초가 지나면 사라지도록
     // 지금은 버튼 누르면 로그 출력만. 기능은 이후 추가해야함
-    private IEnumerator MinionEvent(float repeatTime)
+    private IEnumerator MinionEvent(float _repeatTime)
     {
         while (true)
         {
-            yield return new WaitForSeconds(repeatTime);
+            yield return new WaitForSeconds(_repeatTime);
+            var buttonGo = eventButton.gameObject;
             //상호작용 표시
             switch (SelectRandomEvent())
             {
                 case MinionEnums.EVENT.EXTRA_GEM:
-                    eventButton = Instantiate(eventButtonPrefab, Camera.main.WorldToScreenPoint(transform.position + eventButtonPos),
-                                                Quaternion.identity, GameObject.FindGameObjectWithTag("StaminaCanvas").transform);
-                    eventButton.gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "추가재화";
+                    buttonGo.SetActive(true);
+                    eventBtnText.text = "추가재화";
                     eventButton.onClick.AddListener(() => {
-                        Debug.Log("extragem event");
-                        Destroy(eventButton.gameObject);
+                        Debug.Log("extraGem event");
+                        buttonGo.SetActive(false);
                         });
-                    StartCoroutine(DestroyEventButton());
+                    eventCoroutine[1] = StartCoroutine(EndEvent());
                     break;
                 case MinionEnums.EVENT.TRUST:
-                    eventButton = Instantiate(eventButtonPrefab, Camera.main.WorldToScreenPoint(transform.position + eventButtonPos),
-                                                Quaternion.identity, GameObject.FindGameObjectWithTag("StaminaCanvas").transform);
-                    eventButton.gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "신뢰도";
+                    buttonGo.SetActive(true);
+                    eventBtnText.text = "신뢰도";
                     eventButton.onClick.AddListener(() => {
                         Debug.Log("trust event");
-                        Destroy(eventButton.gameObject);
+                        buttonGo.SetActive(false);
                     });
-                    StartCoroutine(DestroyEventButton());
+                    eventCoroutine[1] = StartCoroutine(EndEvent());
                     break;
                 case MinionEnums.EVENT.FEVER_TIME:
-                    eventButton = Instantiate(eventButtonPrefab, Camera.main.WorldToScreenPoint(transform.position + eventButtonPos),
-                                                Quaternion.identity, GameObject.FindGameObjectWithTag("StaminaCanvas").transform);
-                    eventButton.gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "피버타임";
+                    buttonGo.SetActive(true);
+                    eventBtnText.text = "피버타임";
                     eventButton.onClick.AddListener(() => {
                         Debug.Log("fever event");
-                        Destroy(eventButton.gameObject);
+                        buttonGo.SetActive(false);
                     });
-                    StartCoroutine(DestroyEventButton());
+                    eventCoroutine[1] = StartCoroutine(EndEvent());
                     break;
             }
         }
     }
 
-    private IEnumerator DestroyEventButton()
+    private IEnumerator EndEvent()
     {
         yield return new WaitForSeconds(1.0f);
-        if (eventButton != null)
-        {
-            Destroy(eventButton.gameObject);
-        }
+        eventButton.gameObject.SetActive(false);
     }
 
     /// <summary>
@@ -234,6 +266,39 @@ public class MinionController : MonoBehaviour
             return MinionEnums.EVENT.FEVER_TIME;
         }
     }
+
+    public void TryChangeRestState()
+    {
+        if (restTimer > 0f)
+        {
+            Debug.Log($"쿨타임 {restTimer}초 남음");
+            return;
+        }
+        
+        StartCoroutine(RestTimerStart());
+        if (isActive)
+        {
+            RestMinion();
+            Debug.Log("휴식 시작, 노동 종료");
+        }
+        else if(isResting)
+        {
+            ActivateMinion();
+            Debug.Log("휴식 종료, 노동 시작");
+        }
+    }
+
+    private IEnumerator RestTimerStart()
+    {
+        restTimer = 15;
+        while (restTimer > 0f)
+        {
+            Debug.Log($"restTimer : {restTimer}초");
+            yield return new WaitForSeconds(1.0f);
+            restTimer -= 1;
+        }
+    }
+    
     #endregion
 }
 
