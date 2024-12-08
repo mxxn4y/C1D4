199178cfaf588d70b8ed666e9396493abe5dbe48
2yeb <qls7404@naver.com>
2024-12-08 23:49:19 +0900

    Revert "호버링"
    This reverts commit fb1fb33c3f48a40ef042067b1b9ac1d02b24d09c.

--- Assets/02.Scripts/CollectBook/BookEventManager.cs
+++ Assets/02.Scripts/CollectBook/BookEventManager.cs
@@ -11,7 +11,7 @@ public class BookEventManager : MonoBehaviour,IPointerClickHandler,IPointerEnter
     private CardProperty cardProperty;
     public GameObject cardPropertyPrefab;
     private GameObject currentCardProperty;
-    public int chooseableCount = 3;
+    public int chooseableCount=3;
 
     void Start()
     {
@@ -26,22 +26,22 @@ public class BookEventManager : MonoBehaviour,IPointerClickHandler,IPointerEnter
             RectTransform rectTransform = currentCardProperty.GetComponent<RectTransform>();
             if (rectTransform != null)
             {
-                // ������ �ϴ����� ��ġ ����
+                // 오른쪽 하단으로 위치 조정
                 rectTransform.position = new Vector2(mousePosition.x + 80, mousePosition.y - 80);
             }
         }
     }
 
-        public void OnPointerClick(PointerEventData _eventData) //ī�� Ŭ��, �ε��� Ŭ��
+        public void OnPointerClick(PointerEventData _eventData) //카드 클릭, 인덱스 클릭
         {
             GameObject clickedObj = _eventData.pointerCurrentRaycast.gameObject;
-            CollectCard collectCard = clickedObj.GetComponentInParent<CollectCard>(); // �ݷ�Ʈ���� ��� ī�� �����տ� �����Ǿ��ִ� ��ũ��Ʈ
+            CollectCard collectCard = clickedObj.GetComponentInParent<CollectCard>(); // 콜렉트북의 모든 카드 프리팹에 부착되어있는 스크립트
 
-            Debug.Log("Ŭ���� ������Ʈ: " + clickedObj.name);
+            Debug.Log("클릭된 오브젝트: " + clickedObj.name);
 
             if (clickedObj == null)
             {
-                Debug.Log("����� ������Ʈ �ƹ��͵� ����");
+                Debug.Log("적용될 오브젝트 아무것도 없음");
             }
             else if (clickedObj.TryGetComponent<IndexEvent>(out IndexEvent index))
             {
@@ -52,33 +52,33 @@ public class BookEventManager : MonoBehaviour,IPointerClickHandler,IPointerEnter
             {
 
                 //card.CardClick();
-                Debug.Log($"CollectCard�� Minion ������: {collectCard.minionData.Data.name}");
+                Debug.Log($"CollectCard의 Minion 데이터: {collectCard.minionData.Data.name}");
 
                 if (PlayerData.Instance.SelectedMinions.Contains(collectCard.minionData))
                 {
                     PlayerData.Instance.SelectedMinions.Remove(collectCard.minionData);
                     collectCard.SetUnClickImg();
-                    Debug.Log("���õ� ����Ʈ�� Ŭ�� �̴Ͼ��� ���� ��");
+                    Debug.Log("선택된 리스트에 클릭 미니언이 있을 때");
 
                     foreach (var item in PlayerData.Instance.SelectedMinions)
                     {
-                        Debug.Log("���õ� ����Ʈ���� ���� �̴Ͼ�: " + item.Data.name);
+                        Debug.Log("선택된 리스트에서 남은 미니언: " + item.Data.name);
                     }
                 }
                 else if (PlayerData.Instance.SelectedMinions.Count < chooseableCount)
             {
                     PlayerData.Instance.SelectedMinions.Add(collectCard.minionData);
                     collectCard.SetClickImg();
-                    Debug.Log("���õ� ����Ʈ�� Ŭ�� �̴Ͼ��� ���� ��");
+                    Debug.Log("선택된 리스트에 클릭 미니언이 없을 때");
 
                     foreach (var item in PlayerData.Instance.SelectedMinions)
                     {
-                        Debug.Log("���õ� ����Ʈ���� �߰�:" + item.Data.name);
+                        Debug.Log("선택된 리스트에서 추가:" + item.Data.name);
                     }
                 }
             else
             {
-                Debug.Log("3�������� ���� ����");
+                Debug.Log("3개까지만 선택 가능");
             }
             }
             else
@@ -93,30 +93,26 @@ public class BookEventManager : MonoBehaviour,IPointerClickHandler,IPointerEnter
             GameObject hoveredObj = _eventData.pointerCurrentRaycast.gameObject;
             CollectCard collectCard = hoveredObj.GetComponentInParent<CollectCard>();
 
-            Debug.Log("ȣ������ ������Ʈ: " + hoveredObj.name);
+            Debug.Log("호버링된 오브젝트: " + hoveredObj.name);
 
             if (hoveredObj != null && collectCard != null && collectCard.IsUnlockCard)
             {
-            if(collectCard == null)
-            {
-                Debug.Log("콜렉트카드 없음");
-            }
                 if (cardPropertyPrefab != null)
                 {
                     GameObject cardPropertyObject = Instantiate(cardPropertyPrefab, transform);
                     cardProperty = cardPropertyObject.GetComponent<CardProperty>();
                     currentCardProperty = cardPropertyObject;
                     cardProperty.DisplayCardProperty(collectCard.minionData);
-                    Debug.Log($"ȣ������ ������Ʈ�� Minion ������: {collectCard.minionData.Data.name}");
+                    Debug.Log($"호버링된 오브젝트의 Minion 데이터: {collectCard.minionData.Data.name}");
                 }
                 else
                 {
-                    Debug.LogError("CardPropertyPrefab�� �Ҵ���� ����");
+                    Debug.LogError("CardPropertyPrefab이 할당되지 않음");
                 }
             }
             else
                 {
-                    Debug.Log("ȣ���� ������Ʈ�� �̴Ͼ� ������ ����");
+                    Debug.Log("호버링 오브젝트의 미니언 데이터 없음");
                 }
         }
 
